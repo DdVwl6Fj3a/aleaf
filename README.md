c843d0160ceda0f5d2b2ab1907f5698d96b3b425
eric <eric.y.corsican@gmail.com>
2021-04-02 02:00:26 +0800

    fix build script

--- README.md
+++ README.md
@@ -27,3 +27,7 @@ cd aleaf
 Refer [here](https://mozilla.github.io/firefox-browser-architecture/experiments/2017-09-21-rust-on-android.html) for more details.
 
 Please note this is a debug build, you may easily modify the script `./app/src/main/rust/leaf-android/build.sh` to make a release build.
+
+## Limitation
+
+TLS outbound is not working on `x86_64` due to https://github.com/alexcrichton/openssl-probe/issues/8. But it's possible to specify a custom certificate chain via the [`SSL_CERT_FILE`](https://docs.rs/openssl/0.10.33/openssl/ssl/struct.SslConnectorBuilder.html#method.set_default_verify_paths) env var.
--- app/src/main/rust/leaf-android/build.sh
+++ app/src/main/rust/leaf-android/build.sh
@@ -10,6 +10,7 @@ export CARGO_TARGET_X86_64_LINUX_ANDROID_AR="$CWD/NDK/x86_64/bin/x86_64-linux-an
 export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="$CWD/NDK/x86_64/bin/x86_64-linux-android-clang"
 export PATH="$CWD/NDK/x86_64/bin":$PATH
 cargo build --target x86_64-linux-android --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf-mobile/default-openssl"
+mkdir -p "$BASE/../../jniLibs/x86_64/"
 cp "$BASE/target/x86_64-linux-android/debug/libleafandroid.so" "$BASE/../../jniLibs/x86_64/"
 
 "${NDK_HOME}/build/tools/make_standalone_toolchain.py" --api 26 --arch arm64 --install-dir "$CWD/NDK/arm64"
@@ -17,4 +18,5 @@ export CARGO_TARGET_AARCH64_LINUX_ANDROID_AR="$CWD/NDK/arm64/bin/aarch64-linux-a
 export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$CWD/NDK/arm64/bin/aarch64-linux-android-clang"
 export PATH="$CWD/NDK/arm64/bin":$PATH
 cargo build --target aarch64-linux-android --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf-mobile/default-ring"
+mkdir -p "$BASE/../../jniLibs/arm64-v8a/"
 cp "$BASE/target/aarch64-linux-android/debug/libleafandroid.so" "$BASE/../../jniLibs/arm64-v8a/"
