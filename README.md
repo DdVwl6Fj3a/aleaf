564bda3d18451e21dc4920d654ef23f100b01909
eric <eric.y.corsican@gmail.com>
2021-04-02 09:31:16 +0800

    ci

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Shows how to build a VPN app for Android using [leaf](https://github.com/eycorsi
 
 * Rust
 * GCC/clang
-* Android Studio
+* Make
 * SDK
 * NDK
 
@@ -14,20 +14,18 @@ Shows how to build a VPN app for Android using [leaf](https://github.com/eycorsi
 
 ```
 export ANDROID_HOME=/path/to/sdk
-export NDK_HOME=/path/to/ndk
+export NDK_HOME=/path/to/sdk/ndk-bundle
 
 rustup target add aarch64-linux-android x86_64-linux-android
 
 git clone --recursive https://github.com/eycorsican/aleaf
 cd aleaf
 
-./app/src/main/rust/leaf-android/build.sh
+./app/src/main/rust/leaf-android/build.sh debug
 ```
 
-Refer [here](https://mozilla.github.io/firefox-browser-architecture/experiments/2017-09-21-rust-on-android.html) for more details.
-
-Please note this is a debug build, you may easily modify the script `./app/src/main/rust/leaf-android/build.sh` to make a release build.
+Refer [here](https://mozilla.github.io/firefox-browser-architecture/experiments/2017-09-21-rust-on-android.html) and [here](https://github.com/eycorsican/aleaf/.github/workflows/ci.yml) for more details.
 
 ## Limitation
 
-TLS outbound is not working on `x86_64` due to https://github.com/alexcrichton/openssl-probe/issues/8. But it's possible to specify a custom certificate chain via the [`SSL_CERT_FILE`](https://docs.rs/openssl/0.10.33/openssl/ssl/struct.SslConnectorBuilder.html#method.set_default_verify_paths) env var.
+TLS outbound is not working on `x86_64` due to this [issue](https://github.com/alexcrichton/openssl-probe/issues/8). But it's possible to specify a custom certificate chain via the [`SSL_CERT_FILE`](https://docs.rs/openssl/0.10.33/openssl/ssl/struct.SslConnectorBuilder.html#method.set_default_verify_paths) env var.
--- app/src/main/rust/leaf-android/build.sh
+++ app/src/main/rust/leaf-android/build.sh
@@ -1,22 +1,58 @@
 #!/bin/bash
 
-CWD=`pwd`
+mode=debug
+
+if [ "$1" = "release" ]; then
+	mode=release
+fi
+
 BASE=`dirname "$0"`
+HOST_OS=`uname -s | tr "[:upper:]" "[:lower:]"`
+HOST_ARCH=`uname -m | tr "[:upper:]" "[:lower:]"`
 
-mkdir -p "$CWD/NDK"
+android_tools="$NDK_HOME/toolchains/llvm/prebuilt/$HOST_OS-$HOST_ARCH/bin"
+api=26
 
-"${NDK_HOME}/build/tools/make_standalone_toolchain.py" --api 26 --arch x86_64 --install-dir "$CWD/NDK/x86_64"
-export CARGO_TARGET_X86_64_LINUX_ANDROID_AR="$CWD/NDK/x86_64/bin/x86_64-linux-android-ar"
-export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="$CWD/NDK/x86_64/bin/x86_64-linux-android-clang"
-export PATH="$CWD/NDK/x86_64/bin":$PATH
-cargo build --target x86_64-linux-android --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf-mobile/default-openssl"
-mkdir -p "$BASE/../../jniLibs/x86_64/"
-cp "$BASE/target/x86_64-linux-android/debug/libleafandroid.so" "$BASE/../../jniLibs/x86_64/"
-
-"${NDK_HOME}/build/tools/make_standalone_toolchain.py" --api 26 --arch arm64 --install-dir "$CWD/NDK/arm64"
-export CARGO_TARGET_AARCH64_LINUX_ANDROID_AR="$CWD/NDK/arm64/bin/aarch64-linux-android-ar"
-export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$CWD/NDK/arm64/bin/aarch64-linux-android-clang"
-export PATH="$CWD/NDK/arm64/bin":$PATH
-cargo build --target aarch64-linux-android --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf-mobile/default-ring"
-mkdir -p "$BASE/../../jniLibs/arm64-v8a/"
-cp "$BASE/target/aarch64-linux-android/debug/libleafandroid.so" "$BASE/../../jniLibs/arm64-v8a/"
+for target in x86_64-linux-android aarch64-linux-android; do
+    case $target in
+        'x86_64-linux-android')
+            export CC_x86_64_linux_android="$android_tools/${target}${api}-clang"
+            export AR_x86_64_linux_android="$android_tools/${target}-ar"
+            export CARGO_TARGET_X86_64_LINUX_ANDROID_AR="$android_tools/$target-ar"
+            export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="$android_tools/${target}${api}-clang"
+            export PATH="$NDK_HOME/toolchains/llvm/prebuilt/$HOST_OS-$HOST_ARCH/bin/":$PATH
+            mkdir -p "$BASE/../../jniLibs/x86_64/"
+			case $mode in
+				'release')
+					cargo build --target $target --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf-mobile/default-openssl" --release
+					cp "$BASE/target/$target/release/libleafandroid.so" "$BASE/../../jniLibs/x86_64/"
+					;;
+				*)
+					cargo build --target $target --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf-mobile/default-openssl"
+					cp "$BASE/target/$target/debug/libleafandroid.so" "$BASE/../../jniLibs/x86_64/"
+					;;
+			esac
+            ;;
+        'aarch64-linux-android')
+            export CC_aarch64_linux_android="$android_tools/${target}${api}-clang"
+            export AR_aarch64_linux_android="$android_tools/${target}-ar"
+            export CARGO_TARGET_AARCH64_LINUX_ANDROID_AR="$android_tools/$target-ar"
+            export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$android_tools/${target}${api}-clang"
+            export PATH="$NDK_HOME/toolchains/llvm/prebuilt/$HOST_OS-$HOST_ARCH/bin/":$PATH
+            mkdir -p "$BASE/../../jniLibs/arm64-v8a/"
+			case $mode in
+				'release')
+					cargo build --target $target --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf-mobile/default-openssl" --release
+					cp "$BASE/target/$target/release/libleafandroid.so" "$BASE/../../jniLibs/arm64-v8a/"
+					;;
+				*)
+					cargo build --target $target --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf-mobile/default-openssl"
+					cp "$BASE/target/$target/debug/libleafandroid.so" "$BASE/../../jniLibs/arm64-v8a/"
+					;;
+			esac
+			;;
+        *)
+            echo "Unknown target $target"
+            ;;
+    esac
+done
